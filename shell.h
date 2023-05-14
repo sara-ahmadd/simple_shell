90e28177cf0629831a9af2c7dcb1fbd3cbf69207
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-14 01:23:55 -0700

    sell.h

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+void init();
+void execcmd(char **argv);
+char *getPath(char *command);
+#endif
