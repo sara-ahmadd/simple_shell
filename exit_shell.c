4904fd4da32a8d5904d0b0819b06620f5fc560f4
Sara Ahmad <saraahmad2060039@gmail.com>
2023-05-14 01:58:13 -0700

    exit shell

---
+++ exit_shell.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * exit_shell - exit the shell
+ * @argv: list of arguments
+ *
+ * Return: void
+ */
+
+void exit_shell(char *argv[])
+{
+	if (strcmp(argv[0], "exit") == 0)
+	{
+		exit(0);
+	}
+}
--- shell.h
+++ shell.h
@@ -7,7 +7,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+
 void init();
 void execcmd(char **argv);
 char *getPath(char *command);
+void exit_shell(char *argv[]);
 #endif
